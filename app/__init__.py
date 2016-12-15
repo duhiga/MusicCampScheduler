e11f6fd0ed19da08166cd1086be8c5a5bf73931c
Alex Duhig <alexduhig@gmail.com>
2016-12-15 19:10:32 +0800

    Now notifies users if the day they're looking at isn't scheduled yet.

--- app/__init__.py
+++ app/__init__.py
@@ -183,59 +183,66 @@ def home(userid,inputdate='n'):
         session.close()
         return ('Did not find user in database. You have entered an incorrect URL address.')
 
-    try:
-        log('home fetch requested for user %s' % userid)
-        log('date modifier is currently set to %s' % inputdate)
-        #get the current announcement
-        currentannouncement = session.query(announcement).order_by(desc(announcement.creationtime)).first()
-        if currentannouncement is not None:
-            announcementcontent = currentannouncement.content.replace("\n","<br />")
-        else:
-            announcementcontent = ''
-        #get impontant datetimes
-        today = datetime.datetime.combine(datetime.date.today(), datetime.time.min) #get today's date
-        log('Today is %s' % today.strftime('%Y-%m-%d %H:%M'))
-        campstarttime = datetime.datetime.strptime(getconfig('StartTime'), '%Y-%m-%d %H:%M')
-        campendtime = datetime.datetime.strptime(getconfig('EndTime'), '%Y-%m-%d %H:%M')
-        #if the suer has submitted a date, convert it to a datetime and use it as the display date
-        if inputdate != 'n': 
-            displaydate = datetime.datetime.strptime(inputdate, '%Y-%m-%d')
-        #if the user has not submitted a date and today is before the start of camp, use the first day of camp as the display date
-        elif today < datetime.datetime.strptime(getconfig('StartTime'), '%Y-%m-%d %H:%M'): 
-            displaydate = datetime.datetime.strptime(getconfig('StartTime').split()[0], '%Y-%m-%d')
-        #if today is after the start of camp, use today as the display date
-        else:
-            displaydate = today
+    #try:
+    log('home fetch requested for user %s' % userid)
+    log('date modifier is currently set to %s' % inputdate)
+    #get the current announcement
+    currentannouncement = session.query(announcement).order_by(desc(announcement.creationtime)).first()
+    if currentannouncement is not None:
+        announcementcontent = currentannouncement.content.replace("\n","<br />")
+    else:
+        announcementcontent = ''
+    #get impontant datetimes
+    today = datetime.datetime.combine(datetime.date.today(), datetime.time.min) #get today's date
+    log('Today is %s' % today.strftime('%Y-%m-%d %H:%M'))
+    campstarttime = datetime.datetime.strptime(getconfig('StartTime'), '%Y-%m-%d %H:%M')
+    campendtime = datetime.datetime.strptime(getconfig('EndTime'), '%Y-%m-%d %H:%M')
+    #if the suer has submitted a date, convert it to a datetime and use it as the display date
+    if inputdate != 'n': 
+        displaydate = datetime.datetime.strptime(inputdate, '%Y-%m-%d')
+    #if the user has not submitted a date and today is before the start of camp, use the first day of camp as the display date
+    elif today < datetime.datetime.strptime(getconfig('StartTime'), '%Y-%m-%d %H:%M'): 
+        displaydate = datetime.datetime.strptime(getconfig('StartTime').split()[0], '%Y-%m-%d')
+    #if today is after the start of camp, use today as the display date
+    else:
+        displaydate = today
         
-        previousday = displaydate + datetime.timedelta(days=-1)
-        midday = displaydate + datetime.timedelta(hours=12)
-        nextday = displaydate + datetime.timedelta(days=1)
+    previousday = displaydate + datetime.timedelta(days=-1)
+    midday = displaydate + datetime.timedelta(hours=12)
+    nextday = displaydate + datetime.timedelta(days=1)
 
-        #get an array containing the dates that the camp is running
-        dates = []
-        for d in range((campendtime-campstarttime).days + 2):
-            dates.append(campstarttime + timedelta(days=d))
+    #get an array containing the dates that the camp is running
+    dates = []
+    for d in range((campendtime-campstarttime).days + 2):
+        dates.append(campstarttime + timedelta(days=d))
 
-        #get the user's schedule, an array of objects depending on the user's state during each period
-        schedule = getschedule(session,thisuser,displaydate)
+    #get the user's schedule, an array of objects depending on the user's state during each period
+    schedule = getschedule(session,thisuser,displaydate)
 
-        session.close()
+    unscheduled = False
+    for p in schedule:
+        if session.query(group).filter(group.periodid == p.periodid, group.status == "Confirmed", or_(group.ismusical == 1, group.iseveryone == 1)).first() is None:
+            unscheduled = True
+            break
 
-        return render_template('home.html', \
-                            thisuser=thisuser, \
-                            date=displaydate,\
-                            schedule=schedule, \
-                            dates=dates, \
-                            previousday=previousday, \
-                            nextday=nextday, \
-                            today=today, \
-                            campname=getconfig('Name'), favicon=getconfig('Favicon_URL'), instrumentlist=getconfig('Instruments').split(","), supportemailaddress=getconfig('SupportEmailAddress'), \
-                            currentannouncement=announcementcontent, \
-                            now = datetime.datetime.now(), \
-                            midday=midday, \
-                            )
+    session.close()
 
-    except Exception as ex:
+    return render_template('home.html', \
+                        thisuser=thisuser, \
+                        date=displaydate,\
+                        schedule=schedule, \
+                        dates=dates, \
+                        previousday=previousday, \
+                        nextday=nextday, \
+                        today=today, \
+                        campname=getconfig('Name'), favicon=getconfig('Favicon_URL'), instrumentlist=getconfig('Instruments').split(","), supportemailaddress=getconfig('SupportEmailAddress'), \
+                        currentannouncement=announcementcontent, \
+                        now = datetime.datetime.now(), \
+                        midday=midday, \
+                        unscheduled=unscheduled, \
+                        )
+
+    """except Exception as ex:
         log('Failed to execute %s for user %s %s with exception: %s.' % (request.method, thisuser.firstname, thisuser.lastname, ex))
         message = ('Failed to execute %s with exception: %s. Try refreshing the page and trying again or contact camp administration.' % (request.method, ex))
         session.rollback()
@@ -247,7 +254,7 @@ def home(userid,inputdate='n'):
                                         errormessage = 'Failed to display page with exception: %s.' % ex
                                         )
         else:
-            return jsonify(message = message, url = 'none')
+            return jsonify(message = message, url = 'none')"""
 
 #NOT CURRENTLY USED Currently broken. Doesn't like the fact that the getschedule function returns an array of inconsistant objects. Either make them consistant,
 #or put logic into the 'for s in ...' part of the below function. Or maybe improve the query in the getschedule function.

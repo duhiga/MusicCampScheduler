96ecb04972f92c778715326db5549113381f251c
Alex <alexduhig@gmail.com>
2018-01-05 15:23:03 +0800

    fixed an issue where people leaving during a given period were not included in the listing for that period.

--- app/__init__.py
+++ app/__init__.py
@@ -1458,11 +1458,11 @@ def periodpage(logonid,periodid):
         #grab just the userids of those players to be used in the next query
         players_in_groups_query = session.query(user.userid).\
             join(groupassignment).join(group).join(period).\
-            filter(user.isactive == 1, user.arrival <= thisperiod.starttime, user.departure >= thisperiod.endtime, group.periodid == thisperiod.periodid, group.status == 'Confirmed')
+            filter(user.isactive == 1, user.arrival <= thisperiod.starttime, user.departure >= thisperiod.starttime, group.periodid == thisperiod.periodid, group.status == 'Confirmed')
         #find all other players to be displayed to the user
-        unallocatedplayers = session.query(user.userid, user.firstname, user.lastname, instrument.instrumentname).join(instrument).filter(~user.userid.in_(players_in_groups_query), user.isactive == 1, user.arrival <= thisperiod.starttime, user.departure >= thisperiod.endtime, instrument.isprimary == 1).all()
-        unallocatedplayers_query = session.query(user.userid).join(instrument).filter(~user.userid.in_(players_in_groups_query), user.isactive == 1, user.arrival <= thisperiod.starttime, user.departure >= thisperiod.endtime, instrument.isprimary == 1)
-        nonplayers = session.query(user.userid, user.firstname, user.lastname, sqlalchemy.sql.expression.literal_column("'Non Player'").label("instrumentname")).filter(~user.userid.in_(players_in_groups_query), ~user.userid.in_(unallocatedplayers_query), user.isactive == 1, user.arrival <= thisperiod.starttime, user.departure >= thisperiod.endtime).all()
+        unallocatedplayers = session.query(user.userid, user.firstname, user.lastname, instrument.instrumentname).join(instrument).filter(~user.userid.in_(players_in_groups_query), user.isactive == 1, user.arrival <= thisperiod.starttime, user.departure >= thisperiod.starttime, instrument.isprimary == 1).all()
+        unallocatedplayers_query = session.query(user.userid).join(instrument).filter(~user.userid.in_(players_in_groups_query), user.isactive == 1, user.arrival <= thisperiod.starttime, user.departure >= thisperiod.starttime, instrument.isprimary == 1)
+        nonplayers = session.query(user.userid, user.firstname, user.lastname, sqlalchemy.sql.expression.literal_column("'Non Player'").label("instrumentname")).filter(~user.userid.in_(players_in_groups_query), ~user.userid.in_(unallocatedplayers_query), user.isactive == 1, user.arrival <= thisperiod.starttime, user.departure >= thisperiod.starttime).all()
         thisperiod = session.query(period).filter(period.periodid == periodid).first()
         session.close()
         return render_template('periodpage.html', \
